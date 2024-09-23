42d48380fe71bbaf0158c07e95ef9db83f3a697c
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-23 07:40:42 +0530

    jwt token verify

--- Fetch-Authentication/jwtAuthentication.js
+++ Fetch-Authentication/jwtAuthentication.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const jwtPassword = '123456';
 
 const app = express();
+app.use(express.json()); // to decode the body
 
 // in-memory users for now
 const ALL_USERS = [
@@ -16,35 +17,43 @@ const ALL_USERS = [
   },
 ];
 
-function userExists(username, password){
+function userExists(username, password) {
   // logic to return true/false if this user exists
   // try to do with 'find' function in js
-  for(let i=0 ; i<ALL_USERS.length; i++){
-    if(ALL_USERS[i].username == username && ALL_USERS[i].password == password)
-        return true;
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (ALL_USERS[i].username == username && ALL_USERS[i].password == password)
+      return true;
   }
   return false;
 }
 
-app.post("/signin", (req, res) => {
+app.post('/signin', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if(!userExists(username, password)){
+  if (!userExists(username, password)) {
     return res.status(403).json({
       msg: "User doesn't exists",
     });
   }
 
-  var token = jwt.sign({username: username}, jwtPassword);
+  var token = jwt.sign({ username: username }, jwtPassword);
   return res.json({
     token,
   });
 });
 
-app.get("/users", (req, res) => {
+app.get('/users', (req, res) => {
   const token = req.headers.authorization;
-  try(
+  try {
     const decoded = jwt.verify(token, jwtPassword);
-  )
-})
+    const username = decoded.username;
+    res.json({
+      users: ALL_USERS,
+    });
+  } catch (err) {
+    return res.status(403).json({
+      msg: 'Invalid token',
+    });
+  }
+});
