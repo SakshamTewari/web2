fc1a9ffd291502d5bb372df8e18bfeacbb9f75bb
Saksham Tewari <sakshamtewari3@gmail.com>
2024-09-23 07:31:38 +0530

    Routes Added: '/signin' , '/users'

--- Fetch-Authentication/jwtAuthentication.js
+++ Fetch-Authentication/jwtAuthentication.js
@@ -15,3 +15,36 @@ const ALL_USERS = [
     name: 't2',
   },
 ];
+
+function userExists(username, password){
+  // logic to return true/false if this user exists
+  // try to do with 'find' function in js
+  for(let i=0 ; i<ALL_USERS.length; i++){
+    if(ALL_USERS[i].username == username && ALL_USERS[i].password == password)
+        return true;
+  }
+  return false;
+}
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!userExists(username, password)){
+    return res.status(403).json({
+      msg: "User doesn't exists",
+    });
+  }
+
+  var token = jwt.sign({username: username}, jwtPassword);
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", (req, res) => {
+  const token = req.headers.authorization;
+  try(
+    const decoded = jwt.verify(token, jwtPassword);
+  )
+})
