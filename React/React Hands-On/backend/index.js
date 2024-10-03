c7c6c5d90646d1709eac9d033f0fd225d830d410
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-03 21:28:16 +0530

    Initial backend commit

---
+++ React/React Hands-On/backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+app.get('/todos', (req, res) => {});
+
+app.post('/todo', (req, res) => {});
+
+app.put('/completed', (req, res) => {});
