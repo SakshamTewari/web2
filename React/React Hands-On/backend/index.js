bacd6b717f4685f8979bb8fd8ed3e380a2cc0b34
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-04 21:23:56 +0530

    Updated POST req with validation

--- React/React Hands-On/backend/index.js
+++ React/React Hands-On/backend/index.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import { createTodo, updateTodo } from './validation';
 const app = express();
 
 app.use(express.json());
 
 app.get('/todos', (req, res) => {});
 
-app.post('/todo', (req, res) => {});
+app.post('/todo', (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: 'You sent the wrong inputs',
+    });
+    return;
+  }
+  // put it in mongoDB
+});
 
 app.put('/completed', (req, res) => {});
