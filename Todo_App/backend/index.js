b77c5107761085c78711eb0089f23469b43cdb99
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-05 12:04:36 +0530

    GET, POST, PUT todos with validations

---
+++ Todo_App/backend/index.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import { createTodo, updateTodo } from './validation';
+import todo from './db';
+const app = express();
+
+app.use(express.json());
+
+// GET todos
+app.get('/todos', async (req, res) => {
+  const todos = await todo.find({});
+  res.json({
+    todos,
+  });
+});
+
+// POST todos
+app.post('/todo', async (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: 'You sent the wrong inputs',
+    });
+    return;
+  }
+  // put it in mongoDB
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  res.json({
+    msg: 'Todo Created!!!',
+  });
+});
+
+// PUT todos
+app.put('/completed', async (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: 'You sent the wrong inputs',
+    });
+    return;
+  }
+  // await todo.find({
+  //   title: updatePayload.title,
+  //   description: updatePayload.description,
+  // });
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    },
+  );
+
+  res.json({
+    msg: 'Todo marked as completed',
+  });
+});
