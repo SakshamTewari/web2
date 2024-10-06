15d70e88541fae6227cafbb77b15f5063b9f8a42
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-06 21:20:51 +0530

    'fetch' and POST todo

--- Todo_App/frontend/src/components/CreateTodo.jsx
+++ Todo_App/frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,35 @@
 export function CreateTodo() {
-    return <div>
-        <input type="text" placeholder="title" /><br />
-        <input type="text" placeholder="description" /><br />
 
-        <button>Add Todo</button>
+    // react-query
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    return <div>
+        <input type="text" placeholder="title" onChange={function (e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+        }} /><br />
+        <input type="text" placeholder="description" onChange={function (e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+        }} /><br />
+
+        <button onClick={() => {
+            // axios
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+                .then(async function (res) {
+                    const json = await res.json();
+                    alert("Todo added");
+                })
+        }}>Add Todo</button>
     </div>
 }
