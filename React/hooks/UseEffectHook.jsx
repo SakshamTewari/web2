a77f2b5c9f14a93f9560e52246a1d10a6c05fbff
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-11 15:39:04 +0530

    Added button to update todos

--- React/hooks/UseEffectHook.jsx
+++ React/hooks/UseEffectHook.jsx
@@ -3,22 +3,26 @@ import { useEffect, useState } from "react";
 export function UseEffectHook() {
 
     const [todos, setTodos] = useState({});
+    const [count, setCount] = useState(1);
 
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/todos/${Math.floor(Math.random() * 10)}`)
+        fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
             .then(async (res) => {
                 console.log("res:", res);
                 const json = await res.json();
                 console.log("json:", json);
                 setTodos(json);
             })
-    }, []);
+    }, [count]);
 
-    console.log(todos)
+    function updateCount() {
+        setCount(Math.floor(Math.random() * 100));
+    }
 
     return (
         <div>
+            <button onClick={updateCount}>Update Todos</button>
             <Todos title={todos.title} completed={todos.completed} id={todos.id} />
         </div>
     )
