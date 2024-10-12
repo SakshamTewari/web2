17b7577a50b789f283f75801e7aa5ccc879c29b0
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-12 20:40:48 +0530

    useCallback,memo definitions

--- React/hooks/UseCallback.jsx
+++ React/hooks/UseCallback.jsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useCallback, useState, memo } from "react";
 
 export function UseCallback() {
 
     const [count, setCount] = useState(0);
 
+    /*
+    useCallback makes a function referentially equal.
+    function will only change 'referentially' when some input changes and not when just the parent component re-renders.
+    */
+    const inputFunction = useCallback(() => {
+        console.log("using callback");
+    }, []);
+
     return (
         <div>
-            <ButtonComponent />
+            <ButtonComponent inputFunction={inputFunction} />
             <button onClick={() => {
                 setCount(count + 1);
-            }}></button>
+            }}>Click</button>
         </div>
     )
 }
 
-const ButtonComponent = memo(() => {
+/*
+When a child component is wrapped around 'memo', then that child will only render when its input changes.
+It will not just render when parent component renders
+*/
+
+const ButtonComponent = memo(({ inputFunction }) => {
     console.log("child render");
 
     return <div>
