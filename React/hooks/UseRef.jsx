5ef323b9c602ee4dc41be33c0e267feeff2387f3
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-19 20:58:16 +0530

    update DOM element using useRef

--- React/hooks/UseRef.jsx
+++ React/hooks/UseRef.jsx
@@ -1,14 +1,24 @@
-import { useRef } from "react";
+// useRef is used to access DOM elements in a much better way
+
+import { useEffect, useRef } from "react";
 
 export function UseRef() {
     let ref = useRef(0);
+    let divRef = useRef();
 
     function incrementCount() {
         ref.current++;
         alert("You clicked " + ref.current + " times");
     }
 
+    useEffect(() => {
+        setTimeout(() => {
+            divRef.current.innerHTML = "Updated using useRef";
+        }, 4000);
+    }, []);
+
     return <div>
         <button onClick={incrementCount}>Click me!</button>
+        <p ref={divRef}></p>
     </div>
 }
