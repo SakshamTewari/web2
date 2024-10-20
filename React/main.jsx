4031f027a038a63b5ad78b78759f1f016a515b90
Saksham Tewari <sakshamtewari3@gmail.com>
2024-10-20 16:11:10 +0530

    Routing, BrowserRouter added

--- React/main.jsx
+++ React/main.jsx
@@ -6,6 +6,9 @@ import { UseEffectHook } from './hooks/UseEffectHook.jsx'
 import { UseMemoHook } from './hooks/UseMemoHook.jsx'
 import { UseCallback } from './hooks/UseCallback.jsx'
 import { UseRef } from './hooks/UseRef.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Dashboard } from './components/Dashboard.jsx'
+import { LandingPage } from './components/LandingPage.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -16,5 +19,11 @@ createRoot(document.getElementById('root')).render(
         {/* <UseMemoHook /> */}
         {/* <UseCallback /> */}
         <UseRef />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/landing' element={<LandingPage />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 )
