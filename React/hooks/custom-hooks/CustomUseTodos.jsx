cdcbdc97646e02e27a9c6dc9ae6a599d89355ea6
Saksham Tewari <sakshamtewari3@gmail.com>
2024-11-13 22:06:47 +0530

    useTodos custom hook

---
+++ React/hooks/custom-hooks/CustomUseTodos.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useTransition } from "react";
+
+export function CustomUseTodos() {
+    const { todos, loading } = useTodos();
+
+    return (
+        <>
+            {loading ? "Loading..." : todos}
+        </>
+    )
+
+}
+
+export function useTodos() {
+
+    const [todos, setTodos] = useState([]);
+    const [count, setCount] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+
+            setCount(Math.floor(Math.random() * 10) + 1);
+            fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+                .then(async (res) => {
+                    const json = await res.json();
+                    setTodos(json);
+                    setLoading(false);
+                })
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [count]);
+
+    return { todos, loading };
+
+}
