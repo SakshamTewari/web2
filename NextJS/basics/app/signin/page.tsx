537ea00c9aec39908fd320fdca4e4b5fd193a592
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-07 22:57:39 +0530

    added border , background-color

--- NextJS/basics/app/signin/page.tsx
+++ NextJS/basics/app/signin/page.tsx
@@ -2,7 +2,7 @@ export default function SignIn() {
   return (
     <div className='h-screen flex justify-center flex-col'>
       <div className='flex justify-center'>
-        <a href='#' className='block'>
+        <a href='#' className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100'>
           <div className='px-10'>
             <div className='text-3xl font-extrabold'>Signin</div>
           </div>
