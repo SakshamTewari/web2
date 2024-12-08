f636ac31da3d835a8ea3473a8e8f83d8fda69484
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-08 20:29:18 +0530

    import signup component

--- NextJS/basics/app/(auth)/signup/page.tsx
+++ NextJS/basics/app/(auth)/signup/page.tsx
@@ -1,31 +1,8 @@
+import { Signup } from "@/components/Signup"
+
 export default function SignUp() {
-    return (
-      <div className='h-screen flex justify-center flex-col'>
-        <div className='flex justify-center'>
-          <a href='#' className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100'>
-            <div className='px-10'>
-              <div className='text-3xl font-extrabold'>Signup</div>
-            </div>
-            <div>
-              <LabelledInput label="Username" placeholder="sakshamtewari3@gmail.com" />
-              <LabelledInput label="Password" type={"password"} placeholder="123456" />
-              <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py2.5 me-2 mb-2">Signup</button>
-            </div>
-          </a>
-        </div>
-      </div>
-    );
-  }
-  
-  interface LabelledInputType {
-      label:string;
-      placeholder: string;
-      type?: string;
-  }
-  
-  function LabelledInput({label, placeholder, type}: LabelledInputType){
-      return <div>
-          <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-          <input type={type || "text"} id="first_name" className="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
-      </div>
-  }
+  return <div>
+    <Signup/>
+  </div>
+
+}
