339a5250d53a4642cbe342ff8012f64a8280e060
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-10 00:28:47 +0530

    styles added

--- NextJS/basics/components/UserDetails.tsx
+++ NextJS/basics/components/UserDetails.tsx
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
-export default async function UserDetails(){
-    const userDetails = await getUserData();
+export default async function UserDetails() {
+  await new Promise((r) => setTimeout(r, 3000)); // added delay
+  const userDetails = await getUserData();
 
-    return(
-        <div>
-            Hi, {userDetails.firstName} [{userDetails.email}]
-        </div>
-    )
+  return (
+    <div className='flex items-center justify-center bg-green-50'>
+      <div className='text-center border'></div>
+      Hi, {userDetails.firstName} [{userDetails.email}]
+    </div>
+  );
 }
 
-async function getUserData(){
-    const response = await axios.get(`https://dummyjson.com/users/${Math.floor(Math.random() * 100) + 1}`);
-    console.log(response.data);
-    return response.data;
+async function getUserData() {
+  const response = await axios.get(
+    `https://dummyjson.com/users/${Math.floor(Math.random() * 100) + 1}`,
+  );
+  console.log(response.data);
+  return response.data;
 }
