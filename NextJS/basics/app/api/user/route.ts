ee9c5d12a2c7fd78fc8960de2b881a8ee7925d14
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-16 20:27:39 +0530

    POST handler

--- NextJS/basics/app/api/user/route.ts
+++ NextJS/basics/app/api/user/route.ts
@@ -1,7 +1,20 @@
+import { NextRequest } from 'next/server';
+
 export function GET() {
   // database logic
   return Response.json({
     name: 'Saksham',
     email: 'sakshamtewari3@gmail.com',
+  });
+}
+
+export async function POST(req: NextRequest) {
+  // extract the body
+  const body = await req.json();
+  console.log(body);
+  // store the body in the database
+
+  return Response.json({
+    message: 'You are logged in',
   });
 }
