a10d883949c42dc6f18046f6ec3b74130ec67e31
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-17 23:10:38 +0530

    onClick prop

--- NextJS/basics/components/Button.tsx
+++ NextJS/basics/components/Button.tsx
@@ -1,11 +1,15 @@
-"use client"
+'use client';
 
-export function Button({label}){
-
-    function clickHandler(){
-        console.log("hello");
-    }
-    return <>
-        <button onClick={clickHandler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py2.5 me-2 mb-2">{label}</button>
+export function Button({ label, onClick }) {
+  return (
+    <>
+      <button
+        onClick={onClick}
+        type='button'
+        className='mt-8 w-full text-white bg-gray-800 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py2.5 me-2 mb-2'
+      >
+        {label}
+      </button>
     </>
+  );
 }
