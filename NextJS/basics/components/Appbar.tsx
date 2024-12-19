0ae58a9f0e416b023547cddf0889be943c673b7e
Saksham Tewari <sakshamtewari3@gmail.com>
2024-12-19 19:53:07 +0530

    Appbar component

---
+++ NextJS/basics/components/Appbar.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export function Appbar() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          router.push('/api/auth/signin');
+        }}
+      >
+        Signin
+      </button>
+    </div>
+  );
+}
